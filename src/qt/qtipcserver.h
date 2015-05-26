1e94d925ee0d2a9e92669b1c24aa6d7f371d5ed3
NewSpringCoin <newspringcoin@gmail.com>
2015-05-26 22:27:22 +0300

    Initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define NewSpringCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "NewSpringCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
